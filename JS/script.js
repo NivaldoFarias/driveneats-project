dc4b69a0aa91ceab2496e5e0a92ea15543173516
Nivaldo <badivia.f@gmail.com>
2022-01-28 20:30:15 -0300

    Added singular function for all items

--- JS/script.js
+++ JS/script.js
@@ -58,6 +58,28 @@ function selectDessert(id) {
     allowPurchase(1); // 1 = element de-selected, clear section 
   }
 }
+function selectItem(id, section) {
+  const selected = document.getElementsByClassName("selected");
+  const section = document.getElementsByClassName(section);
+  const element = document.getElementById(id);
+  
+  if (selected.length == 0)
+  {
+    element.classList.toggle("selected");
+    allowPurchase(0);
+  }
+  for (let i = 0; i < selected.length; i++){
+    if (selected[i] == element){
+      deSelectFood(element);
+      allowPurchase(1);
+    }
+    /* else if ()
+    {
+      selected[i].classList.toggle("selected");
+      element.classList.toggle("selected");
+    }  */
+  }
+}
 function allowPurchase(i) {
   const foodOpted = document.getElementsByClassName("selected-food");
   const drinkOpted = document.getElementsByClassName("selected-drink");
@@ -65,7 +87,8 @@ function allowPurchase(i) {
   const selectedItems = (foodOpted.length + drinkOpted.length + dessertOpted.length);
   const element = document.getElementById("btn");
 
-  if (i == 0) {
+  if (!i) // same as if i == 0
+  {
     if (selectedItems == 3){
       toggleButton();
       element.innerHTML = `Fechar pedido`;
@@ -77,41 +100,19 @@ function allowPurchase(i) {
       element.innerHTML = `Selecione ${3 - selectedItems} itens para fechar o pedido`;
     }
   }
-  else if (i == 1) {
+  else if (i) // same as if == 1
+  {
     if (selectedItems == 2){
       toggleButton();
       element.innerHTML = `Selecione ${3 - selectedItems} item para fechar o pedido`;
-      console.log("passou 1");
     }
     else if (selectedItems == 1){
       element.innerHTML = `Selecione ${3 - selectedItems} itens para fechar o pedido`;
     }
     else if (selectedItems == 0){
       element.innerHTML = `Selecione ${3 - selectedItems} itens para fechar o pedido`;
-      console.log("passou 2");
     }
   }
-
-  /*if (i == 0) {
-    if (selectedItems === 3){
-      toggleButton();
-      element.innerHTML = `Fechar pedido`;
-    }
-    else if (selectedItems === 1){
-      element.innerHTML = `Selecione ${3 - selectedItems} itens para fechar o pedido`;
-    }
-    else if (selectedItems === 2){
-      element.innerHTML = `Selecione ${3 - selectedItems} item para fechar o pedido`;
-    }
-  }
-  else if (i == 2) {
-    if (selectedItems === 2){
-      element.innerHTML = `Selecione ${3 - selectedItems} item para fechar o pedido`;
-    }
-    else {
-      element.innerHTML = `Selecione ${3 - selectedItems} itens para fechar o pedido`;
-    }
-  }*/
 }
 function popUpDeSelectDessert(element) {
   const scrollDisable1 = document.querySelector("body");
@@ -127,25 +128,20 @@ function popUpDeSelectDessert(element) {
 }
 function deSelectFood(element) {
   element.classList.remove("selected-food");
-  
 }
 function deSelectDrink(element) {
   element.classList.remove("selected-drink");
 }
 function deSelectDessert(element) {
   element.classList.remove("selected-dessert");
-  allowPurchase();
 }
 function toggleButton() {
   const btn = document.getElementById("btn");
-  console.log(btn.disabled + "0");
   if (btn.disabled) {
     btn.disabled = false;
-    console.log(btn.disabled + "1");
   }
   else if (!btn.disabled){
     btn.disabled = true;
-    console.log(btn.disabled + "2");
   }
 }
 /*function popUpPurchase() {
